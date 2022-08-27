88130b815b3b215baf2b82fa4b273d7f31613f06
S. Bratton Stough <info@duelingworlds.com>
2022-08-27 17:58:59 -0500

    Workers App Connected to AWS RDBS 8/27/2022

--- src/main/java/com/p0a/cameramanbrayton/workers/EmpApp.java
+++ src/main/java/com/p0a/cameramanbrayton/workers/EmpApp.java
@@ -1,9 +1,13 @@
 package com.p0a.cameramanbrayton.workers;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Scanner;
+import org.postgresql.Driver;
 
 class Worker{
     private final int workerNumber; // Worker Number will begin with 100x, whereas x will be a number beginning with 1 to ∞
@@ -41,6 +45,18 @@ class EmpApp {
 
     public static void main(String[] args) {
 
+        String dbUrl = "jdbc:postgresql://java-angular-220815.cuuno41ql544.us-east-1.rds.amazonaws.com:5432/postgres?currentSchema=workersapp";
+        String dbUsername = "scott";
+        String dbPassword = "revature";
+        try {
+            Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            if (conn != null) {
+            System.out.println("Connection successful!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Could not establish a connection");
+            e.printStackTrace();
+        }
         List<Worker> workerCollection = new ArrayList<Worker>();
         try (Scanner s = new Scanner(System.in)) {
             try (Scanner s1 = new Scanner(System.in)) {
